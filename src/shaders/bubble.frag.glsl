8e154c297d959bf7b0e1b1e18ac24c6d5f8d5972
Mederic Carriat <carriatmederic@gmail.com>
2021-07-15 10:36:45 +0200

    Add bugged Perlin 2002 version

--- src/shaders/bubble.frag.glsl
+++ src/shaders/bubble.frag.glsl
@@ -14,12 +14,12 @@ uniform float time;
 
 void main()
 {
-    float ambientStrength = 0.1;
+    float ambientStrength = 1;
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(lightPos - FragPos);
     vec3 viewDir = normalize(viewPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     float spec = pow(max(dot(reflect(-lightDir, norm), viewDir), 0.0), 32);
 
-    FragColor = vec4((ambientStrength + diff + spec) * lightColor * objectColor, 1.0);
+    FragColor = vec4((ambientStrength + diff + spec) * lightColor * Normal, 1.0);
 } 
